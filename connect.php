054dd2c433d2c7ab533bcec9c7243e58041b3940
Ashutosh Thakkar <ashutosh.thakkar@it-ally-u81.svaapta.local>
2022-06-27 15:39:37 +0530

    connection-file

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect('localhost','root','root','user_details');
+// if($conn){
+// 	echo "Connection established";
+// }else{
+// 	die('Could not Connect MySql Server:' .mysql_error());
+// }
+
+
+?>
